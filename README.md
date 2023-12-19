0f2fba48169be32dc3f11e29bb1123bbb80f69db
Jetnik Syla <sjetnik@gmail.com>
2023-12-19 15:01:23 -0500

    password generation functionality

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# Password-Generator
+# Password Generator
+
+*This is a simple web-based password generator. Users can specify the length of the password and choose to include lowercase letters, uppercase letters, numbers, and special characters.*
+
+## How to Use
+
+1. Open `index.html` in a web browser.
+2. Enter the desired length of the password (between 8 and 128 characters).
+3. Choose the character types to include.
+4. Click the "Generate Password" button.
+5. The generated password will appear in the designated area.
+
+## Files and Structure
+
+- `index.html`: HTML file containing the structure of the web page.
+- `style.css`: External stylesheet for styling the web page.
+- `script.js`: JavaScript file with functions for password generation.
+
+## Author:  **JETNIK SYLA**   
+
+## Contact
+
+For inquiries or collaboration opportunities, please feel free to reach out to **Jetnik Syla:**
+
+* #### **Email:** sjetnik@gmail.com
+
+* #### **Phone:** + (1) 914-415-9578
+
+* #### **GitHub Profile:** Jetnik Syla
---
+++ script.js
@@ -0,0 +1,80 @@
+// Function to generate a random password based on user input
+function generatePassword() {
+  let length = parseInt(
+    prompt("Enter the length of the password (between 8 and 128 characters):")
+  );
+
+  // Validate the user input for password length
+  if (isNaN(length) || length < 8 || length > 128) {
+    alert("Please enter a valid password length between 8 and 128 characters.");
+    return;
+  }
+
+  // Ask the user about including different character types in the password
+  let includeLowercase = confirm("Include lowercase letters?");
+  let includeUppercase = confirm("Include uppercase letters?");
+  let includeNumbers = confirm("Include numbers?");
+  let includeSpecialCharacters = confirm("Include special characters?");
+
+  // Validate that at least one character type is selected
+  if (
+    !includeLowercase &&
+    !includeUppercase &&
+    !includeNumbers &&
+    !includeSpecialCharacters
+  ) {
+    alert("At least one character type should be selected.");
+    return;
+  }
+
+  // Generate a random password based on user preferences
+  let password = generateRandomPassword(
+    length,
+    includeLowercase,
+    includeUppercase,
+    includeNumbers,
+    includeSpecialCharacters
+  );
+
+  // Display the generated password in the specified HTML element
+  document.getElementById("Password").value = password;
+}
+
+// Function to generate a random password using specified character sets
+function generateRandomPassword(
+  length,
+  includeLowercase,
+  includeUppercase,
+  includeNumbers,
+  includeSpecialCharacters
+) {
+  // Define character sets for lowercase, uppercase, numbers, and special characters
+  const characterSets = {
+    lowercase: "abcdefghijklmnopqrstuvwxyz",
+    uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    numbers: "0123456789",
+    specialCharacters: "!@#$%^&*()_-+=<>?/",
+  };
+
+  // Initialize an empty string to store valid characters based on user preferences
+  let validCharacters = "";
+
+  // Build the set of valid characters based on user preferences
+  if (includeLowercase) validCharacters += characterSets.lowercase;
+  if (includeUppercase) validCharacters += characterSets.uppercase;
+  if (includeNumbers) validCharacters += characterSets.numbers;
+  if (includeSpecialCharacters)
+    validCharacters += characterSets.specialCharacters;
+
+  // Initialize an empty string for the generated password
+  let password = "";
+
+  // Generate the password by randomly selecting characters from the valid character set
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * validCharacters.length);
+    password += validCharacters[randomIndex];
+  }
+
+  // Return the generated password
+  return password;
+}
